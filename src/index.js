09bf3c484da5b377885632034960d9c17b11e4ed
Arsen Saliev <arsensaliev03@gmail.com>
2019-11-19 22:16:03 +0600

    finished home work

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
 function forEach(array, fn) {
+    for (let i = 0; i < array.length; i++) {
+        fn(array[i], i, array)
+    }
 }
 
 /*
@@ -16,6 +19,13 @@ function forEach(array, fn) {
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
 function map(array, fn) {
+    let res = [];
+
+    for (let i = 0; i < array.length; i++) {
+        res.push(fn(array[i], i, array))
+    }
+
+    return res;
 }
 
 /*
@@ -24,7 +34,16 @@ function map(array, fn) {
  Напишите аналог встроенного метода reduce для работы с массивами
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
+
 function reduce(array, fn, initial) {
+    let prev = initial === undefined ? array[0] : initial;
+    let i = initial === undefined ? 1 : 0;
+
+    for (i; i < array.length; i++) {
+        prev = fn(prev, array[i], i, array);
+    }
+
+    return prev;
 }
 
 /*
@@ -36,6 +55,13 @@ function reduce(array, fn, initial) {
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
 function upperProps(obj) {
+    let res = [];
+
+    for (let items in obj) {
+        res.push(items.toUpperCase());
+    }
+
+    return res;
 }
 
 /*
@@ -45,6 +71,13 @@ function upperProps(obj) {
  Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
  */
 function slice(array, from, to) {
+    let res = [];
+
+    for (let i = from; i <= array.length; i++) {
+        res.push(array[i]);
+    }
+
+    return res;
 }
 
 /*
@@ -53,8 +86,7 @@ function slice(array, from, to) {
  Функция принимает объект и должна вернуть Proxy для этого объекта
  Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
  */
-function createProxy(obj) {
-}
+function createProxy(obj) {}
 
 export {
     forEach,
