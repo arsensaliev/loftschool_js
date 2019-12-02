2e54379c16156a299e63bb87bdea4272790f1aee
Arsen Saliev <arsensaliev03@gmail.com>
2019-12-02 11:17:47 +0600

    update code

--- src/index.js
+++ src/index.js
@@ -79,15 +79,8 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
-    let res = true;
-
-    target.addEventListener('click', () => {
-        if (res) {
-            fn();
-            res = false;
-        } else {
-            target.removeEventListener('click', e => e);
-        }
+    target.addEventListener('click', fn, {
+        once: true
     });
 }
 
